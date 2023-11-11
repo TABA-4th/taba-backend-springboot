03e090052f400a9318ad2e11f0a3e40003a34275
kjungw1025 <kjungw1025@naver.com>
2023-11-12 00:23:46 +0900

    conf: 스프링부트 환경 설정 세팅

---
+++ src/main/java/com/taba/nimonaemo/NimonaemoApplication.java
@@ -0,0 +1,13 @@
+package com.taba.nimonaemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimonaemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimonaemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taba/nimonaemo/NimonaemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taba.nimonaemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimonaemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
