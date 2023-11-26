ff04f972a19901b33bacc3f08b34557f2ee6b884
kjungw1025 <kjungw1025@naver.com>
2023-11-26 11:09:53 +0900

    feat: 두피 타입에 맞는 샴푸 제품을 찾는 컨트롤러 추가

--- src/main/java/com/taba/nimonaemo/product/controller/ScalpCareProductController.java
+++ src/main/java/com/taba/nimonaemo/product/controller/ScalpCareProductController.java
@@ -1,6 +1,7 @@
 package com.taba.nimonaemo.product.controller;
 
 import com.taba.nimonaemo.product.model.dto.request.RequestInputCareDeviceDto;
+import com.taba.nimonaemo.product.model.dto.request.RequestProductDto;
 import com.taba.nimonaemo.product.model.dto.response.ResponseProductDto;
 import com.taba.nimonaemo.product.service.ScalpCareProductService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,6 +19,17 @@ public class ScalpCareProductController {
     private final ScalpCareProductService scalpCareProductService;
 
     /**
+     * 두피 타입에 해당하는 샴푸
+     *
+     * @param dto           해당하는 두피 타입 요청 DTO (필수 : 타입은 한개 이상으로, 그렇지 않으면 ProductNotFoundException)
+     * @return              타입에 맞는 샴푸
+     */
+    @PostMapping("/shampoo")
+    public List<ResponseProductDto> shampoo(@Valid @RequestBody RequestProductDto dto) {
+        return scalpCareProductService.findShampoo(dto);
+    }
+
+    /**
      * 두피 케어 기기
      * 
      * @return              두피 케어 기기
@@ -30,6 +42,7 @@ public class ScalpCareProductController {
     /**
      * 두피 케어 제품 추가
      *
+     * @param dto           요청 body
      */
     @PostMapping("/input")
     public void inputCareDevice(@Valid @RequestBody RequestInputCareDeviceDto dto) {
