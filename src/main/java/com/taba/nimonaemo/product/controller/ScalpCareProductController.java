b624978d3011a6de046e2037cb1a58ed606ad507
kjungw1025 <kjungw1025@naver.com>
2023-12-03 09:19:44 +0900

    style: 두피 타입에 해당하는 샴푸 추출 컨트롤러 JavaDoc 내용 수정

--- src/main/java/com/taba/nimonaemo/product/controller/ScalpCareProductController.java
+++ src/main/java/com/taba/nimonaemo/product/controller/ScalpCareProductController.java
@@ -1,9 +1,14 @@
 package com.taba.nimonaemo.product.controller;
 
+import com.taba.nimonaemo.product.exception.ProductNotFoundException;
 import com.taba.nimonaemo.product.model.dto.request.RequestInputCareDeviceDto;
 import com.taba.nimonaemo.product.model.dto.request.RequestProductDto;
 import com.taba.nimonaemo.product.model.dto.response.ResponseProductDto;
 import com.taba.nimonaemo.product.service.ScalpCareProductService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +26,8 @@ public class ScalpCareProductController {
     /**
      * 두피 타입에 해당하는 샴푸
      *
-     * @param dto           해당하는 두피 타입 요청 DTO (필수 : 타입은 한개 이상으로, 그렇지 않으면 ProductNotFoundException)
-     * @return              타입에 맞는 샴푸
+     * @param dto           해당하는 두피 타입 요청 DTO
+     * @return              타입에 맞는 샴푸 (모두 false 이면, 전체 샴푸 목록 return)
      */
     @PostMapping("/shampoo")
     public List<ResponseProductDto> shampoo(@Valid @RequestBody RequestProductDto dto) {
@@ -31,7 +36,7 @@ public class ScalpCareProductController {
 
     /**
      * 두피 케어 기기
-     * 
+     *
      * @return              두피 케어 기기
      */
     @GetMapping("/device")
