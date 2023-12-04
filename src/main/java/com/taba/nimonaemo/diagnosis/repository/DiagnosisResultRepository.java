d697684506f8db04001f98b62726d2e9716fd375
kjungw1025 <kjungw1025@naver.com>
2023-12-04 15:40:10 +0900

    feat: 특정 진단 결과 삭제하는 레포지토리 추가

--- src/main/java/com/taba/nimonaemo/diagnosis/repository/DiagnosisResultRepository.java
+++ src/main/java/com/taba/nimonaemo/diagnosis/repository/DiagnosisResultRepository.java
@@ -2,8 +2,11 @@ package com.taba.nimonaemo.diagnosis.repository;
 
 import com.taba.nimonaemo.diagnosis.model.entity.DiagnosisResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface DiagnosisResultRepository extends JpaRepository<DiagnosisResult, Long> {
@@ -12,6 +15,12 @@ public interface DiagnosisResultRepository extends JpaRepository<DiagnosisResult
             "(d.diagnosisDate between TO_DATE(:curDate, 'YYYY/MM') and TO_DATE(:nextDate, 'YYYY/MM'))")
     List<DiagnosisResult> findByDate(Long memberId, String curDate, String nextDate);
 
+    @Transactional
+    @Modifying
+    @Query("delete from DiagnosisResult d " +
+            "where d.member.id = :memberId and " +
+                    "d.diagnosisDate LIKE CONCAT(:targetDate, '%')")
+    void deleteByDate(Long memberId, LocalDateTime targetDate);
 
     @Query("select COUNT(*) from DiagnosisResult d " +
             "where d.member.id = (select m.id from Member m " +
